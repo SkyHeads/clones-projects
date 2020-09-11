072a446960daf7f7e0ca8ae874f936ed4ed1b412
SkyHeads <skyheads@live.com>
2020-09-10 23:59:18 -0300

    config to use brave brownser and tsconfig src/*

---
+++ amazon-clone/src/configs/firebase.ts
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBa0DQG1qgLHF76O6vrcRvKCxoH1jpHwPQ",
+  authDomain: "clone-1b28a.firebaseapp.com",
+  databaseURL: "https://clone-1b28a.firebaseio.com",
+  projectId: "clone-1b28a",
+  storageBucket: "clone-1b28a.appspot.com",
+  messagingSenderId: "975008632674",
+  appId: "1:975008632674:web:83867877d21c30e50ee63b",
+  measurementId: "G-Z6RF30QSPP"
+};
