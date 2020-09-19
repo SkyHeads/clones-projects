d2ca7322eeb43ead7c861834977d699799ba7127
SkyHeads <skyheads@live.com>
2020-09-19 19:18:48 -0300

    context api example

--- amazon-clone/src/App.tsx
+++ amazon-clone/src/App.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
-
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-
 import Header from './components/Header';
 import Home from './components/Home';
 import Checkout from './components/Checkout';
+
+import { AppProvider } from './hooks/context';
+// import Products from './components/products';
 
 import GlobalStyle from './styles/global';
 
 const App: React.FC = () => {
   return (
-    <>
+    <AppProvider>
       <Router>
         <Header />
         <Switch>
@@ -23,7 +24,7 @@ const App: React.FC = () => {
         </Switch>
       </Router>
       <GlobalStyle />
-    </>
+    </AppProvider>
   );
 };
 
--- amazon-clone/src/components/Product/index.tsx
+++ amazon-clone/src/components/Product/index.tsx
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
+import { AppContext } from '../../hooks/context';
+import { Types } from '../../hooks/reducers';
 
 import { Container, ProductInfo } from './styles';
 
 interface ProductProps {
+  id?: string;
   title: string;
   image: string;
   price?: number;
   rating: number;
 }
 
-const Product: React.FC<ProductProps> = ({ title, image, price, rating }) => {
+const Product: React.FC<ProductProps> = ({
+  id,
+  title,
+  image,
+  price,
+  rating,
+}) => {
+  const { state, dispatch } = useContext(AppContext);
+  console.log(state);
   return (
     <Container>
       <ProductInfo>
@@ -24,14 +36,24 @@ const Product: React.FC<ProductProps> = ({ title, image, price, rating }) => {
           {Array(rating)
             .fill(rating, 0)
             .map((_: any, i: number) => (
-              <p>🌟</p>
+              <p key={i}>🌟</p>
             ))}
         </div>
       </ProductInfo>
 
       <img src={image} alt="Product" />
 
-      <button type="submit">Add to Basket</button>
+      <button
+        onClick={() => {
+          dispatch({
+            type: Types.Add,
+          });
+        }}
+        type="submit"
+      >
+        Add to Basket
+      </button>
+      {state.shoppingCart}
     </Container>
   );
 };
