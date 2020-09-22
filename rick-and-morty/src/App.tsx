e45ef34f2d798a0facfc359fd848858d7694a79f
SkyHeads <skyheads@live.com>
2020-09-22 00:18:58 -0300

    interface itodo render todos

--- rick-and-morty/src/App.tsx
+++ rick-and-morty/src/App.tsx
@@ -3,23 +3,39 @@ import './App.css';
 
 type FormElem = React.FormEvent<HTMLFormElement>
 
+interface ITodo {
+  text: string;
+  complete: boolean;
+}
+
 const App: React.FC = () => {
   const [value, setValue] = useState<string>('');
-  const [todos, setTodos] = useState<string[]>([]);
+  const [todos, setTodos] = useState<ITodo[]>([]);
 
   const handleSubmit = (e: FormElem): void => {
     e.preventDefault();
-    console.log(value);
+    addTodo(value);
     setValue('');
   }
 
+  const addTodo = (text: string) => {
+    const newTodos: ITodo[] = [...todos, {text, complete: false}];
+    setTodos(newTodos);
+  }
+
+  console.log(todos);
   return (
     <>
       <h1>Todo List</h1>
       <form onSubmit={handleSubmit}>
-        <input type="text" value={value} onChange={e => setValue(e.target.value)} />
+        <input type="text" value={value} onChange={e => setValue(e.target.value)} required/>
         <button type="submit">Add Todo</button>
       </form>
+      {todos.map(todo => (
+        <ul key={todo.text}>
+          <li>{todo.text}</li>
+        </ul>
+      ))}
     </>
   );
 }
