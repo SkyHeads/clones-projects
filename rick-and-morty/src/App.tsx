323673b1a25a553ec056fcda89421d77a38bc58e
SkyHeads <skyheads@live.com>
2020-09-22 00:37:13 -0300

    install eslint + editorconfig + prettier

--- rick-and-morty/src/App.tsx
+++ rick-and-morty/src/App.tsx
@@ -1,7 +1,7 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
-type FormElem = React.FormEvent<HTMLFormElement>
+type FormElem = React.FormEvent<HTMLFormElement>;
 
 interface ITodo {
   text: string;
@@ -12,23 +12,28 @@ const App: React.FC = () => {
   const [value, setValue] = useState<string>('');
   const [todos, setTodos] = useState<ITodo[]>([]);
 
+  const addTodo = (text: string) => {
+    const newTodos: ITodo[] = [...todos, { text, complete: false }];
+    setTodos(newTodos);
+  };
+
   const handleSubmit = (e: FormElem): void => {
     e.preventDefault();
     addTodo(value);
     setValue('');
-  }
-
-  const addTodo = (text: string) => {
-    const newTodos: ITodo[] = [...todos, {text, complete: false}];
-    setTodos(newTodos);
-  }
+  };
 
   console.log(todos);
   return (
     <>
       <h1>Todo List</h1>
       <form onSubmit={handleSubmit}>
-        <input type="text" value={value} onChange={e => setValue(e.target.value)} required/>
+        <input
+          type="text"
+          value={value}
+          onChange={e => setValue(e.target.value)}
+          required
+        />
         <button type="submit">Add Todo</button>
       </form>
       {todos.map((todo: ITodo, index: number) => (
@@ -39,6 +44,6 @@ const App: React.FC = () => {
       ))}
     </>
   );
-}
+};
 
 export default App;
