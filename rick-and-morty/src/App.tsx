d3ad5c75afee74d9581eb59861c8c6e4d622bd59
SkyHeads <skyheads@live.com>
2020-09-22 23:11:16 -0300

    install eslint + editorconfig + prettier

--- rick-and-morty/src/App.tsx
+++ rick-and-morty/src/App.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 const App: React.FC = () => {
   return (
-      <>
+    <>
       <h1>Rick and Morty</h1>
       <p>Pick your favorite episode</p>
-     </>
+    </>
   );
-}
+};
 
 export default App;
