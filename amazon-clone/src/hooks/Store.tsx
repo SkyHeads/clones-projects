33b7dfc6967208c6de82d958e67c9e6b18b8f12f
SkyHeads <skyheads@live.com>
2020-10-03 18:31:16 -0300

    remove from basket reducer ready to use

--- amazon-clone/src/hooks/Store.tsx
+++ amazon-clone/src/hooks/Store.tsx
@@ -20,9 +20,23 @@ function reducer(state: IState, action: IAction): IState {
     case 'ADD_TO_BASKET':
       return { ...state, basket: [...state.basket, action.payload] };
     case 'REMOVE_FROM_BASKET':
+      const index = state.basket.findIndex(
+        basketItem => basketItem.id === action.payload.id,
+      );
+
+      const newBasket = [...state.basket];
+
+      if (index >= 0) {
+        newBasket.splice(index, 1);
+      } else {
+        console.warn(
+          `Voçe não pode remover o produto (id: ${action.payload.id}), pois ele não está na cesta`,
+        );
+      }
+
       return {
         ...state,
-        basket: [...state.basket.filter(item => item.id !== action.payload.id)],
+        basket: newBasket,
       };
     default:
       return state;
